a10c03452de28064ddcc08680bf7c88c9bc7aa9f
JuliaDaniR <julidrz@hotmail.com.ar>
2025-05-21 18:58:19 -0300

    Agregando data

--- data.js
+++ data.js
@@ -6,8 +6,12 @@ const tareasPorSemana = {
     "Miércoles", "Ingles - Ejercicios b y c de la página 18",
     "Miércoles", "Matemáticas - Revisar los problemas de lógica",
     "Jueves", "",
+    "Vernes", "Comunicación - Entregar el trabajo práctico sobre el texto de Spinelli",
     "Viernes", "Comunicación - Leer el archivo del Classroom - Resúmen de Teorias de la comunicación",
   ],
+  22: [
+    "Miércoles", "Matemáticas - Hacer los ejercicios de propiedades de la potencia desde el 1 al 36 de la primer página",
+  ]
   // Podés seguir agregando semanas
 };
 
@@ -25,13 +29,16 @@ const resumenClasesPorSemana = {
     },
     "Gestión de Software I": {
       temas: [
-   
+        "Finalizamos el trabajo práctico de Business",
       ],
-      enlace: "" // No disponible
+      enlace: "https://drive.google.com/file/d/1ySzLwqHbX1tPv1NIqaE1w4LtmLpUEaH-/view?usp=sharing" 
     },
     "Matemática": {
       temas: [
-      
+        "Corrección de los problemas de lógica",
+        "Comenzamos a ver el tema de Propiedades de la potencia",
+        "Realizamos ejercicios de propiedades de la potencia",
+        "Hicimos hasta el ejercicio 30 de la primer página",
       ]
     },
     "Análisis de Sistemas Organizacionales": {
@@ -41,9 +48,7 @@ const resumenClasesPorSemana = {
       enlace: ""
     },
     "Comunicación": {
-      temas: [
-    
-      
+      temas: [      
       ]
     },
     "Psicosociología de las Organizaciones": {
@@ -55,12 +60,16 @@ const resumenClasesPorSemana = {
     },
     "Arquitectura de las Computadoras": {
       temas: [
-       
+       "Reapaso de los componentes de la computadora",
+       "El profesor nos mostró cómo colocar componentes en la placa madre",
       ]
     },
     "Inglés Técnico I": {
       temas: [
-       
+       "Entregamos la actividad de la página 18",
+       "Conversamos sobre la diferencia entre el inglés británico y el americano",
+       "Realizamos ejercicios de las páginas 16, 17 y 7",
+       "Revisar la lista de temas que puso la profesora en el Classroom para el examen, para consultar si hay algo que no se entendía",
       ],
       enlace: ""
     }
